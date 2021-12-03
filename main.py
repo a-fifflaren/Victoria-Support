5ef8a428510843b40fea9be73d2a0dee80a9bc41
Koshelev A <add.koshelev@gmail.com>
2021-12-03 23:34:04 +0300

    first commit

--- main.py
+++ main.py
@@ -3,25 +3,26 @@ from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.dispatcher.filters import Text
 from aiogram.utils import executor
-from config import TOKEN  # Подключения api токена
+from config import BOT_TOKEN  # Подключения api токена
 
-bot = Bot(token=TOKEN)
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler(commands=['start', 'help'])
 async def command_start(message: types.Message):
-    await bot.send_message(message.from_user.id, "Добро пожаловать {0.first_name}!".format(message.from_user), reply_markup=nav.mainMenu)
+    await bot.send_message(message.from_user.id,
+                           "Добро пожаловать {0.first_name}!\nМеня зовут Виктория!")
 
 
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.reply("Список команд ещё не готов!")
+@dp.message_handler(commands=['eat'])
+async def command_eat(message: types.Message):
+    await bot.send_message(message.from_user.id, "У нас есть два ресторана с Европейской и Итальянской кухней! Какую предпочитаете вы?")
 
 
-@dp.message_handler(Text(equals=['Номерной фонд', 'Наши рестораны', 'Экскурсии', 'Фитнес', 'Технические вопросы', 'Трансфер']))
-async def process_menu(message: types.Message):
-    await message.answer(f'Вы выбрали {message.text}!')
+# @dp.message_handler(Text(equals=['Номерной фонд', 'Наши рестораны', 'Экскурсии', 'Фитнес', 'Технические вопросы', 'Трансфер']))
+# async def process_menu(message: types.Message):
+#   await message.answer(f'Вы выбрали {message.text}!')
 
 
 # @dp.message_handler()
